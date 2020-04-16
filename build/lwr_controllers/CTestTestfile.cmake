fdcc19ee80902914ce6ab8d063a2fa3a83f68376
kren6 <kren6@jhu.edu>
2020-04-15 20:54:04 -0400

    clean

--- build/lwr_controllers/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shimin/ROS_Prgm_Prj/src/kuka-lwr/lwr_controllers
-# Build directory: /home/shimin/ROS_Prgm_Prj/build/lwr_controllers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build/lwr_description/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shimin/ROS_Prgm_Prj/src/kuka-lwr/lwr_description
-# Build directory: /home/shimin/ROS_Prgm_Prj/build/lwr_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
