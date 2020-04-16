e3728e4a4add2a1b4f151cf6a9c923aabf2c067a
kren6 <kren6@jhu.edu>
2020-04-15 20:56:08 -0400

    clean src

--- src/debug_tool/rqt_ez_publisher/README.md
+++
@@ -1,5 +0,0 @@
-rqt_ez_publisher
-================
-
-read http://wiki.ros.org/rqt_ez_publisher
-
--- src/debug_tool/rqt_ez_publisher/src/rqt_ez_publisher/quaternion_module/__init__.py
+++
@@ -1,3 +0,0 @@
-from .quaternion_module import QuaternionModule
-from .rpy_value_widget import RPYValueWidget
-from .rpy_widget import RPYWidget
