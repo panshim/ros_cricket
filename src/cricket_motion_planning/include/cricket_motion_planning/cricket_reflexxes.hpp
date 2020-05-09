74799bf24c822752d622a31c67e779604a8c2afe
Shimin Pan <panshimin0903@outlook.com>
2020-05-09 15:44:45 -0400

    change velocity

--- src/cricket_motion_planning/include/cricket_motion_planning/cricket_reflexxes.hpp
+++ src/cricket_motion_planning/include/cricket_motion_planning/cricket_reflexxes.hpp
@@ -1,6 +1,7 @@
 #ifndef _CRICKET_REFLEXXES_H_
 #define _CRICKET_REFLEXXES_H_
 
+// C++
 #include <iostream>
 #include <string>
 // ROS
@@ -8,6 +9,7 @@
 #include <geometry_msgs/Pose.h>
 #include <tf/tf.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/JointState.h>
 
 // Reflexxes Motion Planning Library
 #include <ReflexxesAPI.h>
@@ -15,13 +17,20 @@
 #include <RMLPositionInputParameters.h>
 #include <RMLPositionOutputParameters.h>
 
+// KDL
+#include <kdl/chainfksolvervel_recursive.hpp>
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/jntarrayvel.hpp>
+#include <kdl/framevel.hpp>
+
+// self-made
 #include "tf_operation.hpp"
 #include "lwr_controllers/PoseRPY.h"
 
 // defines
-#define CYCLE_TIME_IN_SECONDS   0.005 // 1/n Hz traj. command frequency
+#define CYCLE_TIME_IN_SECONDS   0.1 // 1/n Hz traj. command frequency
 #define NUMBER_OF_DOFS          6   // 6 DOF EndEffector
-
+#define Pi                      3.14159265358979323846
 class CricketReflexxes
 {
     private:
@@ -33,13 +42,27 @@ class CricketReflexxes
         lwr_controllers::PoseRPY inv_pos_next_cmd;
         tf::Transform batTransform;
         tf::Transform eeTransform;
+        
         // Publisher & Subscriber
         ros::Subscriber sub_reflex_target;
         ros::Subscriber sub_reflex_twist;
+        ros::Subscriber sub_jointstate;
         ros::Publisher pub_next_cart;
         ros::Publisher pub_inv_next_cart;
+
         // Set-up a timer with a period of one millisecond
         ros::Timer timer;
+        ros::Time prevTime;
+        ros::Time nowTime;
+        double intervalTime;
+        
+        // KDL: Forward Velocity
+        KDL::Tree tree;
+        KDL::Chain chain;
+ 	    KDL::ChainFkSolverVel_recursive* solv_fd_vel;
+        KDL::JntArrayVel jntArrayVel;
+        KDL::FrameVel frameVel;
+
         // Creating all relevant objects of the Type II Reflexxes Motion Library
         RMLPositionInputParameters  *IP;
         RMLPositionOutputParameters *OP;
@@ -53,8 +76,9 @@ class CricketReflexxes
         void TimerCallback(const ros::TimerEvent&);
         void SubMsgCallbackPose(const geometry_msgs::Pose rcv_msg);
         void SubMsgCallbackTwist(const geometry_msgs::Twist rcv_msg);
+        void SubMsgCallbackJnt(const sensor_msgs::JointState recv_js);
         template<class T> static void QuaternionToRPY(const T Quat, double &roll, double &pitch, double &yaw);
         void RPYToQuaternion(tf::Quaternion &Quat, double roll, double pitch, double yaw);
-
+        double RotationPrincipleValue(double rad);//switch rotation to [-pi,pi]
 };
 #endif
--- src/cricket_motion_planning/src/cricket_reflexxes.cpp
+++ src/cricket_motion_planning/src/cricket_reflexxes.cpp
@@ -2,13 +2,15 @@
 
 CricketReflexxes::CricketReflexxes(ros::NodeHandle nh):nh(nh), tf_oper(nh)
 {
-    // Subscriber & Publisher
+    /* Step1: Subscriber & Publisher */
     //topic: [reflexxes_target_cart] --> {ReflexML} --> [cartesian_pos_cmd] --> {Inverse Postion Kine}.
     sub_reflex_target = nh.subscribe<geometry_msgs::Pose>("reflexxes_target_cart", 1, &CricketReflexxes::SubMsgCallbackPose, this);
     sub_reflex_twist = nh.subscribe<geometry_msgs::Twist>("reflexxes_target_twist", 1, &CricketReflexxes::SubMsgCallbackTwist, this);
-    pub_next_cart = nh.advertise<geometry_msgs::Pose>("cartesian_pos_cmd", 10);
+    sub_jointstate = nh.subscribe<sensor_msgs::JointState>("/lwr/joint_states", 1, &CricketReflexxes::SubMsgCallbackJnt, this);
+    pub_next_cart = nh.advertise<geometry_msgs::Pose>("cartesian_pos_cmd", 1);
     pub_inv_next_cart = nh.advertise<lwr_controllers::PoseRPY>("/lwr/one_task_inverse_kinematics/command", 1);
-    // Reflexxes Motion Lib Call
+    
+    // Step2: Reflexxes Motion Lib Call
     IP=new RMLPositionInputParameters(NUMBER_OF_DOFS);
     OP=new RMLPositionOutputParameters(NUMBER_OF_DOFS);
     RML=new ReflexxesAPI(NUMBER_OF_DOFS, CYCLE_TIME_IN_SECONDS);
@@ -16,19 +18,13 @@ CricketReflexxes::CricketReflexxes(ros::NodeHandle nh):nh(nh), tf_oper(nh)
     ResultValue = 0;
     /* TF Operation class initialization in Initial List */
 
-    /* Cricket Bat Transformmation */
-    tf::Vector3 batVec(0,0,-0.4);
-    tf::Quaternion batQuat(0,0,0,1);
-    batTransform.setOrigin(batVec);
-    batTransform.setRotation(batQuat);
-
-    /* set MAX ARGUMENTS for Reflexxes Call */
+    // /* Step3: set MAX ARGUMENTS for Reflexxes Call */
     IP->MaxVelocityVector->VecData          [0] = 2000.0;
     IP->MaxVelocityVector->VecData          [1] = 2000.0;
     IP->MaxVelocityVector->VecData          [2] = 2000.0;
-    IP->MaxVelocityVector->VecData          [3] = 3140.0;
-    IP->MaxVelocityVector->VecData          [4] = 3140.0;
-    IP->MaxVelocityVector->VecData          [5] = 3140.0;
+    IP->MaxVelocityVector->VecData          [3] = 314.0;
+    IP->MaxVelocityVector->VecData          [4] = 314.0;
+    IP->MaxVelocityVector->VecData          [5] = 314.0;
 
     IP->MaxAccelerationVector->VecData      [0] = 5000000.0;
     IP->MaxAccelerationVector->VecData      [1] = 5000000.0;
@@ -37,12 +33,12 @@ CricketReflexxes::CricketReflexxes(ros::NodeHandle nh):nh(nh), tf_oper(nh)
     IP->MaxAccelerationVector->VecData      [4] = 5000000.0;
     IP->MaxAccelerationVector->VecData      [5] = 5000000.0;
     
-    IP->MaxJerkVector->VecData              [0] = 1000000000.0;
-    IP->MaxJerkVector->VecData              [1] = 1000000000.0;
-    IP->MaxJerkVector->VecData              [2] = 1000000000.0;
-    IP->MaxJerkVector->VecData              [3] = 1000000000.0;
-    IP->MaxJerkVector->VecData              [4] = 1000000000.0;
-    IP->MaxJerkVector->VecData              [5] = 1000000000.0;
+    IP->MaxJerkVector->VecData              [0] = 10000000000.0;
+    IP->MaxJerkVector->VecData              [1] = 10000000000.0;
+    IP->MaxJerkVector->VecData              [2] = 10000000000.0;
+    IP->MaxJerkVector->VecData              [3] = 10000000000.0;
+    IP->MaxJerkVector->VecData              [4] = 10000000000.0;
+    IP->MaxJerkVector->VecData              [5] = 10000000000.0;
 
     IP->SelectionVector->VecData            [0] = true;
     IP->SelectionVector->VecData            [1] = true;
@@ -50,9 +46,12 @@ CricketReflexxes::CricketReflexxes(ros::NodeHandle nh):nh(nh), tf_oper(nh)
     IP->SelectionVector->VecData            [3] = true;
     IP->SelectionVector->VecData            [4] = true;
     IP->SelectionVector->VecData            [5] = true;
+    
 
-    /* set Initial CURRENT POSITION for Reflexxes Call */
+    /* Step4: set Initial CURRENT POSITION for Reflexxes Call */
     // Note: For the very first motion after starting the controller, velocities & acceleration are commonly set to zero.
+    prevTime = ros::Time::now();
+    nowTime = prevTime;
     tf_oper.TFListen("/lwr_7_link", "/lwr_base_link");
     double roll, pitch, yaw;
     geometry_msgs::TransformStamped geo_ST;
@@ -82,6 +81,32 @@ CricketReflexxes::CricketReflexxes(ros::NodeHandle nh):nh(nh), tf_oper(nh)
     IP->CurrentAccelerationVector->VecData  [3] = 0;
     IP->CurrentAccelerationVector->VecData  [4] = 0;
     IP->CurrentAccelerationVector->VecData  [5] = 0;
+
+    /* Step5: Cricket Bat Transformmation */
+    tf::Vector3 batVec(0,0,-0.4);
+    tf::Quaternion batQuat(0,0,0,1);
+    batTransform.setOrigin(batVec);
+    batTransform.setRotation(batQuat);
+
+    /* Step6: Forward Velocity Solver */
+    std::string robot_description_string;
+    nh.param("robot_description", robot_description_string, std::string());
+    if(kdl_parser::treeFromString(robot_description_string, tree))
+    {
+        if(tree.getChain("lwr_base_link", "lwr_7_link", chain))
+        {
+            solv_fd_vel = new KDL::ChainFkSolverVel_recursive(chain);
+        }
+        else
+        {
+            ROS_ERROR("Cannot get a chain!");
+        }
+    }
+    else
+    {
+        ROS_ERROR("Cannot get a tree!");
+    }
+    jntArrayVel.resize(chain.getNrOfJoints());
 }
 
 CricketReflexxes::~CricketReflexxes()
@@ -124,17 +149,68 @@ void CricketReflexxes::SubMsgCallbackTwist(const geometry_msgs::Twist rcv_msg)
     IP->TargetVelocityVector->VecData       [5] = 0;
 }
 
+void CricketReflexxes::SubMsgCallbackJnt(const sensor_msgs::JointState recv_js)
+{
+    for(int i=0; i<chain.getNrOfJoints(); i++)
+    {
+        jntArrayVel.q(i) = recv_js.position[i];
+        jntArrayVel.qdot(i) = recv_js.velocity[i];
+    }
+}
 
 void CricketReflexxes::TimerCallback(const ros::TimerEvent& event)
 {
-    // step1: Calling the Reflexxes OTG algorithm: Calc Module's Output: OP
+    // step1: Update the Module's Input: IP
+    tf_oper.TFListen("/lwr_base_link", "/lwr_7_link");
+    double roll, pitch, yaw;
+    geometry_msgs::TransformStamped geo_ST;
+    transformStampedTFToMsg(tf_oper.listen_transform, geo_ST);
+    geometry_msgs::Quaternion Quat(geo_ST.transform.rotation);
+    CricketReflexxes::QuaternionToRPY<geometry_msgs::Quaternion>(Quat, roll, pitch, yaw);
+
+    // Update Current State
+    // nowTime = ros::Time::now();
+    // intervalTime = (nowTime - prevTime).toSec();
+    // prevTime = nowTime;
+    // IP->CurrentVelocityVector->VecData      [0] = (geo_ST.transform.translation.x - IP->CurrentPositionVector->VecData[0])/intervalTime;
+    // IP->CurrentVelocityVector->VecData      [1] = (geo_ST.transform.translation.y - IP->CurrentPositionVector->VecData[1])/intervalTime;
+    // IP->CurrentVelocityVector->VecData      [2] = (geo_ST.transform.translation.z - IP->CurrentPositionVector->VecData[2])/intervalTime;
+    // IP->CurrentVelocityVector->VecData      [3] = (RotationPrincipleValue(roll )- RotationPrincipleValue(IP->CurrentPositionVector->VecData[3]))/intervalTime;
+    // IP->CurrentVelocityVector->VecData      [4] = (RotationPrincipleValue(pitch)- RotationPrincipleValue(IP->CurrentPositionVector->VecData[4]))/intervalTime;
+    // IP->CurrentVelocityVector->VecData      [5] = (RotationPrincipleValue(yaw  )- RotationPrincipleValue(IP->CurrentPositionVector->VecData[5]))/intervalTime;
+    
+    solv_fd_vel->JntToCart(jntArrayVel, frameVel);
+    IP->CurrentVelocityVector->VecData      [0] = frameVel.GetTwist().vel[0];
+    IP->CurrentVelocityVector->VecData      [1] = frameVel.GetTwist().vel[1];
+    IP->CurrentVelocityVector->VecData      [2] = frameVel.GetTwist().vel[2];
+    IP->CurrentVelocityVector->VecData      [3] = frameVel.GetTwist().rot[0];
+    IP->CurrentVelocityVector->VecData      [4] = frameVel.GetTwist().rot[1];
+    IP->CurrentVelocityVector->VecData      [5] = frameVel.GetTwist().rot[2];
+
+    IP->CurrentPositionVector->VecData      [0] = geo_ST.transform.translation.x;
+    IP->CurrentPositionVector->VecData      [1] = geo_ST.transform.translation.y;
+    IP->CurrentPositionVector->VecData      [2] = geo_ST.transform.translation.z;
+    IP->CurrentPositionVector->VecData      [3] = roll;
+    IP->CurrentPositionVector->VecData      [4] = pitch;
+    IP->CurrentPositionVector->VecData      [5] = yaw;
+    // *IP->CurrentVelocityVector      =*OP->NewVelocityVector;
+    *IP->CurrentAccelerationVector  =*OP->NewAccelerationVector;
+
+    // step2: Calling the Reflexxes OTG algorithm: Calc Module's Output: OP
     ResultValue =   RML->RMLPosition(*IP, OP, Flags);
     if (ResultValue < 0)
     {
         ROS_ERROR("ERROR Calc RMLPosition ---- error code: (%d).\n", ResultValue );
+        std::cout << "IntervalTime: " << intervalTime << std::endl;
+        std::cout << "OP->Vec[0]" << OP->NewPositionVector->VecData[0] << std::endl;
+        std::cout << "OP->Vec[1]" << OP->NewPositionVector->VecData[1] << std::endl;
+        std::cout << "OP->Vec[2]" << OP->NewPositionVector->VecData[2] << std::endl;
+        std::cout << "OP->Vec[3]" << OP->NewPositionVector->VecData[3] << std::endl;
+        std::cout << "OP->Vec[4]" << OP->NewPositionVector->VecData[4] << std::endl;
+        std::cout << "OP->Vec[5]" << OP->NewPositionVector->VecData[5] << std::endl;
     }
 
-    // step2: publish on topic: "cartesian_pos_cmd"
+    // step3: publish on topic: "cartesian_pos_cmd"
     tf::Quaternion tf_quat;
     RPYToQuaternion(tf_quat, OP->NewPositionVector->VecData[3], OP->NewPositionVector->VecData[4], OP->NewPositionVector->VecData[5]);
     
@@ -143,10 +219,8 @@ void CricketReflexxes::TimerCallback(const ros::TimerEvent& event)
     eeTransform.setOrigin(eeVec);
     eeTransform.setRotation(tf_quat);
 
-    // std::cout << "before: " << eeTransform.getOrigin().getZ() << std::endl;
+    // hitting point is on the "Bat", not the "End-Effector"
     eeTransform = eeTransform * batTransform;
-    // std::cout << "after: " << eeTransform.getOrigin().getZ() << std::endl;
-    // std::cout << "----------------------------------" << std::endl;
 
     //Construct topic: "cartesian_pos_cmd"
     cartesian_pos_next_cmd.position.x = eeTransform.getOrigin().getX();
@@ -157,17 +231,9 @@ void CricketReflexxes::TimerCallback(const ros::TimerEvent& event)
     cartesian_pos_next_cmd.orientation.z = eeTransform.getRotation().getZ();
     cartesian_pos_next_cmd.orientation.w = eeTransform.getRotation().getW();
     pub_next_cart.publish(cartesian_pos_next_cmd);
-
-    // cartesian_pos_next_cmd.position.x = OP->NewPositionVector->VecData[0];
-    // cartesian_pos_next_cmd.position.y = OP->NewPositionVector->VecData[1];
-    // cartesian_pos_next_cmd.position.z = OP->NewPositionVector->VecData[2];
-    // cartesian_pos_next_cmd.orientation.x = tf_quat.getX();
-    // cartesian_pos_next_cmd.orientation.y = tf_quat.getY();
-    // cartesian_pos_next_cmd.orientation.z = tf_quat.getZ();
-    // cartesian_pos_next_cmd.orientation.w = tf_quat.getW();
-    // pub_next_cart.publish(cartesian_pos_next_cmd);
     
-    // Step 2.1(backup): publish on topic: "/lwr/one_task_inverse_kinematics/command"
+
+    // Step 3.1(backup): publish on topic: "/lwr/one_task_inverse_kinematics/command"
     inv_pos_next_cmd.position.x = OP->NewPositionVector->VecData[0];
     inv_pos_next_cmd.position.y = OP->NewPositionVector->VecData[1];
     inv_pos_next_cmd.position.z = OP->NewPositionVector->VecData[2];
@@ -175,23 +241,6 @@ void CricketReflexxes::TimerCallback(const ros::TimerEvent& event)
     inv_pos_next_cmd.orientation.pitch = OP->NewPositionVector->VecData[4];
     inv_pos_next_cmd.orientation.yaw = OP->NewPositionVector->VecData[5];
     pub_inv_next_cart.publish(inv_pos_next_cmd);
-
-    // step3: Update the Module's Input: IP
-    tf_oper.TFListen("/lwr_base_link", "/lwr_7_link");
-    double roll, pitch, yaw;
-    geometry_msgs::TransformStamped geo_ST;
-    transformStampedTFToMsg(tf_oper.listen_transform, geo_ST);
-    geometry_msgs::Quaternion Quat(geo_ST.transform.rotation);
-    CricketReflexxes::QuaternionToRPY<geometry_msgs::Quaternion>(Quat, roll, pitch, yaw);
-    // CricketReflexxes::QuaternionToRPY<tf::Quaternion>(tf_oper.listen_transform.getRotation(), roll, pitch, yaw);
-    IP->CurrentPositionVector->VecData      [0] = geo_ST.transform.translation.x;
-    IP->CurrentPositionVector->VecData      [1] = geo_ST.transform.translation.y;
-    IP->CurrentPositionVector->VecData      [2] = geo_ST.transform.translation.z;
-    IP->CurrentPositionVector->VecData      [3] = roll;
-    IP->CurrentPositionVector->VecData      [4] = pitch;
-    IP->CurrentPositionVector->VecData      [5] = yaw;
-    *IP->CurrentVelocityVector      =*OP->NewVelocityVector;
-    *IP->CurrentAccelerationVector  =*OP->NewAccelerationVector;
 }
 
 
@@ -207,4 +256,13 @@ template<class T> void CricketReflexxes::QuaternionToRPY(T Quat, double &roll, d
 void CricketReflexxes::RPYToQuaternion(tf::Quaternion &Quat, double roll, double pitch, double yaw)
 {
     Quat.setRPY(roll, pitch, yaw);
+}
+
+double CricketReflexxes::RotationPrincipleValue(double rad)
+{
+    while(rad<-Pi)
+        rad += 2*Pi;
+    while(rad>Pi)
+        rad -= 2*Pi;
+    return rad;
 }
